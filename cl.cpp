bf77fc069cc1907f22c5d94d540c83750435c333
vanshagr19 <vansh1909agrawal@gmail.com>
2026-09-13 18:29:26 +0530

    Auto‑commit 2026-09-13T12:59:25.998Z

--- cl.cpp
+++ cl.cpp
@@ -9,16 +9,47 @@ class Node{
         next = NULL;
     }
 };
-
 class Circularlist{
     Node* head;
     Node* tail;
 
 public:
+    Circularlist(){
     head = tail =NULL;
-    
+    }
+
+    void pushathead(int val){
+        Node* newnode = new Node(val);
+        if (head == NULL){
+            head = tail =newnode;
+            tail ->next = head;
+        }
+        else{
+            newnode ->next = head;
+            head = newnode;
+            tail->next = head;
+        }
+    }
+
+    void print(){
+        Node* temp = head ;
+        if (head == tail){
+            cout << head->data<<"->";
+            
+        }
+        while (temp->next != head){
+            cout<<temp->data<<" -> ";
+            temp = temp->next;
+        }     
+        cout << " NULL ";
+    }
 };
 int main(){
-    
+    Circularlist cl;
+    cl.pushathead(1);
+    cl.pushathead(2);
+    cl.pushathead(3); 
+    cl.pushathead(4);
+    cl.print();
     return 0;
 }
