dac793cac72103e79ba549b992adc5be3748c652
vanshagr19 <vansh1909agrawal@gmail.com>
2026-09-13 19:03:34 +0530

    Auto‑commit 2026-09-13T13:33:34.323Z

--- cl.cpp
+++ cl.cpp
@@ -44,8 +44,39 @@ public:
         }
     }
 
+    void deleteathead(){
+        if (head == NULL){
+            cout<<"no node available";
+            return ;
+        }
+        else if(head == tail){
+            delete head;
+            head = NULL;
+            tail = NULL;
+            return ;
+        }
+        
+        else{
+            // using temp as the temprary pointer
+            Node*temp = head;
+            head = head->next;
+            tail->next = head ;
+            delete temp;
+
+            // without using any temporary pointer 
+            // tail->next = head->next;
+            // head ->next = NULL;
+            // delete head ;
+            // head = tail->next;
+        }
+    }
+
     void print(){
         Node* temp = head ;
+        if (head == NULL){
+            cout << "no node in thr linked list";
+            return ;
+        }
         
         while (temp->next != head){
             cout<<temp->data<<" -> ";
@@ -59,10 +90,15 @@ int main(){
     Circularlist cl;
     cl.pushathead(2);
     cl.pushathead(1);
+
+    // cl.print();
     
     cl.pushattail(3);
     cl.pushattail(4);
-    
+
+    cl.deleteathead();
+    cl.deleteathead();
+
     cl.print();
     return 0;
 }
